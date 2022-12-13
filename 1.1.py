1aefa2fd6df6f1588ecc555c3740c8d95ba9431f
Nate Fox <nfox@byu.edu>
2022-12-13 14:47:01 -0700

    finished 1.1

--- 1.1.py
+++ 1.1.py
@@ -1,3 +1,5 @@
+import time
+
 """
 Given an array of integers, return an array such that each element at i
 is the product of every element in the original array excluding the
@@ -16,12 +18,32 @@ def get_product_array(nums):
     return products
 """
 
+"""
 def get_product_array(nums):
     product = reduce(lambda x, y: x * y, nums, 1)
     products = [product / i for i in nums]
     return products
+"""
 
-print(get_product_array(range(1, 6)))
-print(get_product_array(range(3, 0, -1)))
-#print(get_product_array(range(1, 1000)))
+def get_product_array(nums):
+    n = len(nums)
+    left_side = [1] * n
+    right_side = [1] * n
+    product = 1
+    for i in range(n):
+        left_side[i] = product
+        product *= nums[i]
+    product = 1
+    for i in range(n-1, -1, -1):
+        right_side[i] = product
+        product *= nums[i]
+    products = []
+    for i in range(n):
+        products.append(left_side[i] * right_side[i])
+    return products
+
+start = time.time()
+get_product_array([2] * 100000)
+end = time.time()
+print(end-start, 'seconds')
 
